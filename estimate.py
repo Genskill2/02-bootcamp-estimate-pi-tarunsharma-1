3a21ca5a3e34bd420466e68b9af35a88b253bd52
Tarun Sharma <itsmetarun2000@gmail.com>
2021-06-01 09:28:01 +0530

    initial commit

--- estimate.py
+++ estimate.py
@@ -1,12 +1,38 @@
+import random
 import math
 import unittest
+
+
+def wallis(n):
+    pi = 1
+    for i in range(1,n+1):
+        pi = pi*((4*i**2)/((4*i**2)-1))
+    return(2*pi)
+
+def distance(x,y):
+    msd = (x**2+y**2)**0.5
+    return(msd)
+
+def monte_carlo(n):
+    c = 0
+    s = 0
+    for i in range(n):
+        x = random.random()
+        y = random.random()
+        dis = distance(x,y)
+        s += 1
+        if dis <1 :
+            c += 1
+    pi = 4 * (c/s)
+    return(pi)
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
+
     def test_high_iters(self):
         for i in range(500, 600):
             pi = wallis(i)
@@ -17,7 +43,7 @@ class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
+
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
@@ -26,7 +52,7 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+
+
 if __name__ == "__main__":
     unittest.main()
